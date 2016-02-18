b1e54ac0cae5caa79b44af25d810760b65b985c0
omarnoir <omardiabira@gmail.com>
2016-02-18 23:35:38 +0100

    ajout de la page screen.php avec les infos dedans

---
+++ screen.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+     <head>
+        <title>website of Eric and Omar-Yann</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="website of Eric and Omar-Yann" />
+        <meta name="keywords" content="web,site,depannage, réseau, réseaux, informatique, ordinateur, formation, assistance, problème, internet" />
+        <meta name="author" content="Eric Lorand and Omar-Yann Diabira" />
+        <link href="css/header-style.css" type="text/css" rel="stylesheet" />
+        <link href="css/style.css" type="text/css" rel="stylesheet" />
+        <link href="css/body-style.css" type="text/css" rel="stylesheet" />
+        <link href="css/form-style.css" type="text/css" rel="stylesheet" />
+     </head>
+
+     <body>
+        <?php include("part/header.php"); ?>
+            
+        <section id="section2">
+            <div class="preconseil">
+                <h1>Un problème avec votre ecran ?</h1>
+                <p>Avant d'avoir recours à un dépanneur, nous vous conseillons fortement d'avoir au minimum fait les vérifications listées ci-après, cela peux vous permettre d'identifier le problème sans avoir recours à un dépanneur. Sinon, contactez nous au 06.XX.XX.XX.XX</p>   
+            </div>
+            <div class="conseil">
+                <h1>1</h1> 
+                <h2>Vérifier le cable reliant le l'écran au PC (DVI, HDMI, VGA)</h2>
+
+            </div>
+          
+            <h2>Conseil n°2 : Vérifier le cable d'alimentation de l'ordinateur</h2>
+            <p>Reliant le PC à la prise éléctrique. Si possiblre, essayer une autre prise... ajouter des images aussi.</p>
+            <h2>Conseil N°3</h2>
+       </section>
+            
+        <script src="js/classie.js"></script>
+		<script src="js/cbpAnimatedHeader.min.js"></script>
+        
+     </body>
+</html>

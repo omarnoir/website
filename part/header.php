83abb9309bf875e1247a17c60fcda2c8e563b8a7
omarnoir <omardiabira@gmail.com>
2016-02-18 23:35:02 +0100

    mise du header dans un fichier séparé pour le réutiliser sur plusieurs pages

---
+++ part/header.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="cbp-af-header">
+        <div class="cbp-af-inner">
+            <h1>Omar&ko</h1>
+            <nav>
+            <a href="#">Nous joindre</a>
+            <a href="#">Formation</a>
+            <a href="#">Préstations</a>
+            <a href="#">Zone d'intervention</a>
+        </nav>
+    </div>
+</div>
